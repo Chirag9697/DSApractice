67a2a76f6fadfbcd2ac93913a43c0de0c08f4559
chirag <chirru.ra@gmail.com>
2023-06-04 11:31:12 +0530

    fix lru cache

--- javacodes/Lrucode.java
+++ javacodes/Lrucode.java
@@ -5,9 +5,9 @@ class Node{
     int key;
     Node next;
     Node prev;
-    Node(int data,int key){
-        data=data;
-        key=key;
+    Node(int _data,int _key){
+        data=_data;
+        key=_key;
         next=null;
         prev=null;
     }
@@ -50,14 +50,14 @@ class Lru{
         addnode(new Node(key,data));
         // System.out.print("hello");
         hs.put(key,head.next);
-        System.out.print(head.next.next.data);     
+        // System.out.print(head.next.next.data);     
     }
     void removenode(Node n){
         n.prev.next=n.next;
         n.next.prev=n.prev;
     }
     void addnode(Node n){
-        System.out.print("hello");
+        // System.out.print("hello");
         n.next=head.next;
         head.next.prev=n;
         head.next=n;
@@ -69,11 +69,11 @@ class Lrucode
 	public static void main(String[] args) {
         Lru l=new Lru(3);
         l.put(1,1);
-        System.out.print(l.get(1));
-        // l.put(3,15);
-        // l.put(2,12);
-        // l.put(4,20);
-        // System.out.print(l.get(2));
-        // l.put(4,25);
+        System.out.println(l.get(1));
+        l.put(3,15);
+        l.put(2,12);
+        l.put(4,20);
+        System.out.println(l.get(2));
+        l.put(4,25);
 	}
 }
